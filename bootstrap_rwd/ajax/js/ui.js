35cb68deb7c35a5e357a91158322061ac230d0aa
minseogo <gominseo1112@gmail.com>
2024-06-24 14:39:04 +0900

    자기소개서 페이지 헤더 추가 및 수정

--- bootstrap_rwd/ajax/js/ui.js
+++ bootstrap_rwd/ajax/js/ui.js
@@ -1,14 +1,20 @@
 $(function(){
-    $(window).on('scroll', function(){ // this를 사용할땐 화살표 함수 => 를 사용할 수 없다. 화살표함수는 심플한 식에만 사용하기 //
+    $(window).on('scroll', function(){
         let scrps = $(this).scrollTop();
-        if( scrps > 80 ){ // 스크롤이 아래로
-            $("#hd").addClass('bg-dark border-bottom border-white').removeClass("bg-white")
-        }else{
-            $("#hd").removeClass('bg-dark border-bottom border-white').addClass("bg-white")
+        if (scrps > 80) { // 스크롤이 아래로
+            // $("#hd").addClass('bg-dark border-bottom border-white').removeClass("bg-white");
+            // $("#hd li a").addClass("text-white").removeClass("text_black")
+
+            $("body").addClass("scroll")
+           
+            
+            
+        } else {
+            // $("#hd").removeClass('bg-dark border-bottom border-white').addClass("bg-white");
+            // $("#hd li a").addClass("text-white").removeClass("text_black")
+            $("body").removeClass("scroll")
         }
 
-        
         // $("body").attr("data-scrollps", $(this).scrollTop() )
-
-    })
-})
+    });
+});
